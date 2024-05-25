e0b9adcd064c2c7a46d8bf3d77e194b4da630ab3
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-25 18:14:49 +0500

    working on ui

---
+++ Client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      primary: "#1476ff",
+      secondry : "#f3f5ff",
+      light : "#f9faff"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
